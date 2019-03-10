8699d5df193d1316ab91010046593455272aa110
Mohamad <mohmad_akkad@hotmail.com>
2019-03-10 09:29:27 +0000

    changes on add.py

--- myapp/add.py
+++ myapp/add.py
@@ -1,16 +1,24 @@
 from langdetect import detect
+from django.views.decorators.csrf import csrf_exempt
+import simplejson as json
+import pandas as pd
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseServerError
+import os
 
-
-def classifiy(request):
+@csrf_exempt
+def add(request):
    if request.method == "POST":
     json_data = json.loads(request.body)
     try:
         data = json_data['Tweet']
     except KeyError:
         HttpResponseServerError("Malformed data!")
-    lang = detect(data[0]['text'])
-    df = data[0]['text'] + ',' + data[0]['class']
-    with open('data-'+lang+'.csv', 'a') as f:
-        df.to_csv(f, header=False)
+    lang = detect(data['text'])
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    my_file = os.path.join(THIS_FOLDER, 'data-'+lang+'.csv')
+    df = u'"'+data['text']+ '"'+ ',' + data['class'] + '\n'
+    with open(my_file, 'a',encoding="utf-8") as f:
+        f.write(df)
 
-   return HttpResponse(json.dumps({'ture'}))
+   return HttpResponse({'data-'+lang})
